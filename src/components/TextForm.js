2d1901bffa17a459d670f012c3665c66243e83ec
Mahikagarg09 <mahika_garg@yahoo.com>
2022-09-22 00:25:46 +0530

    fixed error

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -54,7 +54,7 @@ export default function TextForm(props) {
             </div>
             <div className="container my-3" style={{color:props.mode==='light' ? 'black' : 'white'}}>
                 <h2>Your Text Summary</h2>
-                <p>{text.split().filter((element) =>{return element.length!==0}).length} words and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters </p>
                 <p>{0.008 * text.split(/\s+/).filter((element) =>{return element.length!==0}).length } minutes taken to read</p>
                 <h2>Preview</h2>
                 <p>{text.length>0?text:"Enter some text to preview it here"}</p>
