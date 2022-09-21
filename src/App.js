41f8a498ddf9032fe57c009f73f82d5985975f88
Mahikagarg09 <mahika_garg@yahoo.com>
2022-09-21 12:44:40 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React,{ useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-import About from './components/About';
+
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  //Link
-} from "react-router-dom";
 
 
 
@@ -42,20 +36,13 @@ function App() {
 
   return (
     <>
-    <Router>
+    
       <Navbar title="TextUtils "mode={mode} toggleMode={toggleMode} />
       <Alert  alert={alert}/>
       <div className="container my-3">
-          <Switch>
-            <Route exact path="/About">
-              <About mode={mode} />
-            </Route>
-              <Route  exact path="/">
             <TextForm heading="Enter the text to analyze" mode={mode}  showAlert={showAlert}/>
-            </Route>
-            </Switch>
         </div>
-      </Router>
+      
     </>
   );
 }
